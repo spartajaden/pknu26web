b21029e529ab2202f0bcb797ba81e57bdceb7dfa
spartajaden <jaden758@naver.com>
2026-03-24 17:22:07 +0900

    8일차 커밋

---
+++ ex08-03.js
@@ -0,0 +1,25 @@
+// let inp = prompt("값을 입력하세요.");//
+let inp = 1;
+num = Number(inp); // * 1
+console.log("(num = num + 1) + 순서}");
+console.log("후위연산자(num++)");
+console.log(` num 값이 ${num}일때`);
+console.log(`num++ 일때 ${num++}`);
+console.log(`num++ 일때 ${num++}`);
+console.log(`num++ 일때 ${num++}`);
+console.log("====================");
+num = Number(inp); // 초기화
+console.log("전위연산자(++num)");
+console.log(` num 값이 ${num}일때`);
+console.log(`++num 일때 ${++num}`);
+console.log(`++num 일때 ${++num}`);
+console.log(`++num 일때 ${++num}`);
+console.log("====================");
+num = Number(inp); // 초기화
+console.log("연속 테스트(++num, num++)");
+console.log(` num 값이 ${num}일때`);
+console.log(`num++ 일때 ${num++}`);
+console.log(`++num 일때 ${++num}`);
+console.log(`num-- 일때 ${num--}`);
+console.log(`--num 일때 ${--num}`);
+console.log("====================");
